f8bab4c8ead39c773e00d38638fb06c9057d8361
Azad Tamal <77025434+MrAzadT@users.noreply.github.com>
2021-07-01 21:38:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const result = document.getElementById("result");
+const submit = document.getElementById("submit");
+
+function binary(e) {
+  e.preventDefault();
+
+  const num = document.getElementById("number").value;
+
+  if (num === " ") {
+    alert("Please Enter a Number");
+  } else if (num < 0) {
+    alert("Please Enter a Positive Number");
+  } else {
+    result.style.visibility = "visible";
+  }
+
+  let binaryNumber = Number(num).toString(2);
+  result.innerHTML = binaryNumber;
+}
+submit.addEventListener("click", binary);
